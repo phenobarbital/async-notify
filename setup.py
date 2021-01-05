306b9194c9d149a6755d1cf3ebf2a1bd2960ea05
Jesus Lara <jesuslarag@gmail.com>
2021-01-05 14:36:33 +0100

    fixing setup for Notify

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-"""Notify
+"""Notify.
+
     Asynchronous library for sending notifications, used by Navigator.
 See:
 https://bitbucket.org/mobileinsight1/query_api/src/master/
@@ -22,7 +23,14 @@ setup(
     author='Jesus Lara',
     author_email='jlara@trocglobal.com',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    setup_requires=[
+        "wheel==0.36.2",
+        "Cython==0.29.21",
+        "numpy==1.19.4",
+        "asyncio==3.4.3"
+    ],
     install_requires=[
+    'wheel==0.36.2',
     'numpy==1.19.4',
     'asyncio==3.4.3',
     'twilio==6.48.0',
@@ -39,12 +47,19 @@ setup(
     'asyncdb',
     'navconfig'
     ],
+    tests_require=[
+            'pytest>=5.4.0',
+            'coverage',
+            'pytest-asyncio==0.14.0',
+            'pytest-xdist==2.1.0',
+            'pytest-assume==2.4.2'
+    ],
     dependency_links=[
         'git+https://github.com/phenobarbital/asyncdb.git@master#egg=asyncdb',
         'git+https://github.com/phenobarbital/NavConfig.git@main#egg=navconfig'
     ],
     project_urls={  # Optional
-        'Source': 'https://bitbucket.org/mobileinsight1/query_api/',
+        'Source': 'https://bitbucket.org/mobileinsight1/notify/',
         'Funding': 'https://paypal.me/phenobarbital',
         'Say Thanks!': 'https://saythanks.io/to/phenobarbital',
     },
