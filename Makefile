a214166508653031df46dee796fef2aaa1e708d1
Jesus Lara <jesuslarag@gmail.com>
2022-03-01 22:52:56 +0100

    New version of Notify

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+CODE_DIRS := notify/ tests/ examples/
+
 venv:
 	python3.9 -m venv .venv
 	echo 'run `source .venv/bin/activate` to start develop asyncDB'
@@ -19,8 +21,10 @@ format:
 lint:
 	python -m pylint --rcfile .pylint notify/*.py
 	python -m black --check notify
+	flake8 $(CODE_DIRS)
 
 test:
+	tox
 	python -m coverage run -m tests
 	python -m coverage report
 	python -m mypy notify/*.py
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
     Asynchronous library for sending notifications, used by Navigator.
 See:
-https://bitbucket.org/mobileinsight1/query_api/src/master/
+  https://github.com/phenobarbital/async-notify
 """
 
 from setuptools import setup, find_packages
@@ -11,8 +11,8 @@ from setuptools import setup, find_packages
 setup(
     name='notify',
     version=open("VERSION").read().strip(),
-    url='hhttps://github.com/MobileInsight/navigator-notify/',
-    description='Asyncio Notification library with webSockets support',
+    url='https://github.com/phenobarbital/async-notify',
+    description='Asyncio Notification library',
     long_description='Asynchronous library for send notifications to users, used by Navigator',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -21,7 +21,7 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     author='Jesus Lara',
-    author_email='jlara@trocglobal.com',
+    author_email='jesuslara@phenobarbital.info',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     setup_requires=[
         "wheel==0.37.0",
@@ -35,39 +35,9 @@ setup(
         'APScheduler==3.7.0',
         'asyncio==3.4.3',
         'uvloop==0.16.0',
-        'outcome>=1.1.0',
-        'pillow>=8.0.1',
-        'httplib2>=0.20.1',
-        'oauthlib==3.1.1',
-        'PySocks==1.7.1',
-        'google-auth>=2.3.0',
-        'google-auth-httplib2>=0.1.0',
-        'gmail==0.6.3',
-        'pyo365==0.1.3',
-        'slixmpp==1.7.0',
-        'pyshorteners==1.0.1',
-        'twilio==6.48.0',
-        'telegram==0.0.1',
-        'aioimaplib==0.9.0',
-        'aiosmtplib==1.1.6',
-        'tweepy==3.9.0',
-        'tzlocal>=2.1',
-        'pytz>=2021.3',
-        'decorator>=5.1.0',
-        'regex>=2021.9.30',
-        'soupsieve>=2.2.1',
-        'urllib3>=1.26.6',
-        'requests>=2.25.0',
-        'requests[socks]>=2.25.1',
-        "botocore==1.19.29",
-        "boto3==1.16.29",
-        'six>=1.16.0',
-        'pyasn>=1.6.1',
-        'rsa>=4.7.2',
-        'proxylists @ git+https://github.com/phenobarbital/proxylist@main#egg=proxylists',
-        'python-telegram-bot @ git+https://github.com/phenobarbital/python-telegram-bot.git',
-        'asyncdb @ git+https://github.com/phenobarbital/asyncdb.git@master#egg=asyncdb',
-        'navconfig @ git+https://github.com/phenobarbital/NavConfig.git@main#egg=navconfig',
+        'proxylists @ git+https://github.com/phenobarbital/proxylists@main#egg=proxylists',
+        'asyncdb @ git+https://github.com/phenobarbital/asyncdb.git@fix-versions#egg=asyncdb',
+        'navconfig @ git+https://github.com/phenobarbital/NavConfig.git@new-version#egg=navconfig',
     ],
     tests_require=[
             'pytest>=5.4.0',
