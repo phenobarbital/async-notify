1bbb25e95342a24af32674558e02b3ec548eb573
Jesus Lara <jesuslarag@gmail.com>
2023-03-08 20:16:06 +0100

    new version of setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ See:
 """
 import ast
 from os import path
-
 from setuptools import find_packages, setup
 
 
@@ -28,56 +27,52 @@ with open(version, 'r', encoding='utf-8') as meta:
         if len(node.targets) == 1:
             name = node.targets[0]
             if isinstance(name, ast.Name) and \
-                    name.id in {
+                    name.id in (
                         '__version__',
                         '__title__',
                         '__description__',
                         '__author__',
-                        '__license__', '__author_email__'}:
-                v = node.value
-                if name.id == '__version__':
-                    __version__ = v.s
-                if name.id == '__title__':
-                    __title__ = v.s
-                if name.id == '__description__':
-                    __description__ = v.s
-                if name.id == '__license__':
-                    __license__ = v.s
-                if name.id == '__author__':
-                    __author__ = v.s
-                if name.id == '__author_email__':
-                    __author_email__ = v.s
+                        '__license__', '__author_email__'):
+                        v = node.value
+                        if name.id == '__version__':
+                            __version__ = v.s
+                        if name.id == '__title__':
+                            __title__ = v.s
+                        if name.id == '__description__':
+                            __description__ = v.s
+                        if name.id == '__license__':
+                            __license__ = v.s
+                        if name.id == '__author__':
+                            __author__ = v.s
+                        if name.id == '__author_email__':
+                            __author_email__ = v.s
 
 setup(
     name=__title__,
     version=__version__,
+    python_requires=">=3.9.16",
     url='https://github.com/phenobarbital/async-notify',
     description=__description__,
     long_description=readme(),
     long_description_content_type='text/markdown',
-    python_requires=">=3.9.16",
+    license=__license__,
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Operating System :: POSIX :: Linux",
-        "Environment :: Web Environment",
-        "License :: OSI Approved :: BSD License",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Communications",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Framework :: AsyncIO",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Framework :: AsyncIO',
+        'License :: OSI Approved :: BSD License',
     ],
-    keywords=['aiogram', 'asyncio', 'aioimaplib', 'aiobotocore'],
     author='Jesus Lara',
     author_email='jesuslara@phenobarbital.info',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+    packages=find_packages(exclude=["docs", "tests", "settings"]),
     setup_requires=[
-        "wheel==0.38.4",
-        "Cython==0.29.33"
+        'wheel==0.38.4',
+        'cython==0.29.33'
     ],
     install_requires=[
         'asyncio==3.4.3',
