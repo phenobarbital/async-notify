a5fdffd9520149636b8aa9795097c69891a45961
Jesus Lara <jesuslarag@gmail.com>
2023-03-08 23:35:33 +0100

    check if this version works with github action

--- notify/version.py
+++ notify/version.py
@@ -5,7 +5,7 @@ __description__ = (
     "Library for send notifications. "
     "simple and powerful asyncio-based library for sending notifications."
 )
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 __author__ = "Jesus Lara"
 __author_email__ = "jesuslara@phenobarbital.info"
-__license__ = "APACHE"
+__license__ = "BSD"
